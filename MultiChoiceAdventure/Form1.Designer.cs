e103b20e11f52140891de626f8f8c86949e7b920
brenroy999 <brenroy999@ed.amdsb.ca>
2018-11-09 12:20:57 -0500

    Add project files.

---
+++ MultiChoiceAdventure/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace MultiChoiceAdventure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.blueText = new System.Windows.Forms.Label();
+            this.redText = new System.Windows.Forms.Label();
+            this.labelM = new System.Windows.Forms.Label();
+            this.labelB = new System.Windows.Forms.Label();
+            this.labelN = new System.Windows.Forms.Label();
+            this.yellowText = new System.Windows.Forms.Label();
+            this.labelDebug = new System.Windows.Forms.Label();
+            this.labelImage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(12, 9);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(86, 17);
+            this.outputLabel.TabIndex = 0;
+            this.outputLabel.Text = "OutputLabel";
+            // 
+            // blueText
+            // 
+            this.blueText.AutoSize = true;
+            this.blueText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blueText.Location = new System.Drawing.Point(63, 363);
+            this.blueText.Name = "blueText";
+            this.blueText.Size = new System.Drawing.Size(83, 17);
+            this.blueText.TabIndex = 1;
+            this.blueText.Text = "Blue Output";
+            // 
+            // redText
+            // 
+            this.redText.AutoSize = true;
+            this.redText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.redText.Location = new System.Drawing.Point(63, 327);
+            this.redText.Name = "redText";
+            this.redText.Size = new System.Drawing.Size(81, 17);
+            this.redText.TabIndex = 3;
+            this.redText.Text = "Red Output";
+            // 
+            // labelM
+            // 
+            this.labelM.AutoSize = true;
+            this.labelM.BackColor = System.Drawing.Color.Red;
+            this.labelM.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelM.Location = new System.Drawing.Point(35, 330);
+            this.labelM.Margin = new System.Windows.Forms.Padding(9, 3, 9, 3);
+            this.labelM.Name = "labelM";
+            this.labelM.Size = new System.Drawing.Size(16, 13);
+            this.labelM.TabIndex = 4;
+            this.labelM.Text = "M";
+            // 
+            // labelB
+            // 
+            this.labelB.AutoSize = true;
+            this.labelB.BackColor = System.Drawing.Color.Blue;
+            this.labelB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelB.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelB.Location = new System.Drawing.Point(35, 366);
+            this.labelB.Margin = new System.Windows.Forms.Padding(9, 3, 9, 3);
+            this.labelB.Name = "labelB";
+            this.labelB.Size = new System.Drawing.Size(14, 13);
+            this.labelB.TabIndex = 5;
+            this.labelB.Text = "B";
+            // 
+            // labelN
+            // 
+            this.labelN.AutoSize = true;
+            this.labelN.BackColor = System.Drawing.Color.Yellow;
+            this.labelN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelN.Location = new System.Drawing.Point(35, 293);
+            this.labelN.Margin = new System.Windows.Forms.Padding(9, 3, 9, 3);
+            this.labelN.Name = "labelN";
+            this.labelN.Size = new System.Drawing.Size(15, 13);
+            this.labelN.TabIndex = 7;
+            this.labelN.Text = "N";
+            // 
+            // yellowText
+            // 
+            this.yellowText.AutoSize = true;
+            this.yellowText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yellowText.Location = new System.Drawing.Point(63, 290);
+            this.yellowText.Name = "yellowText";
+            this.yellowText.Size = new System.Drawing.Size(95, 17);
+            this.yellowText.TabIndex = 6;
+            this.yellowText.Text = "Yellow Output";
+            // 
+            // labelDebug
+            // 
+            this.labelDebug.AutoSize = true;
+            this.labelDebug.Location = new System.Drawing.Point(565, 294);
+            this.labelDebug.Name = "labelDebug";
+            this.labelDebug.Size = new System.Drawing.Size(66, 13);
+            this.labelDebug.TabIndex = 8;
+            this.labelDebug.Text = "DEBUG BOI";
+            // 
+            // labelImage
+            // 
+            this.labelImage.BackColor = System.Drawing.Color.Transparent;
+            this.labelImage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelImage.ForeColor = System.Drawing.Color.Coral;
+            this.labelImage.Image = global::MultiChoiceAdventure.Properties.Resources.cave;
+            this.labelImage.Location = new System.Drawing.Point(-1, -2);
+            this.labelImage.Name = "labelImage";
+            this.labelImage.Size = new System.Drawing.Size(697, 404);
+            this.labelImage.TabIndex = 9;
+            this.labelImage.Text = "                ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 399);
+            this.Controls.Add(this.labelDebug);
+            this.Controls.Add(this.labelN);
+            this.Controls.Add(this.yellowText);
+            this.Controls.Add(this.labelB);
+            this.Controls.Add(this.labelM);
+            this.Controls.Add(this.redText);
+            this.Controls.Add(this.blueText);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.labelImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label blueText;
+        private System.Windows.Forms.Label redText;
+        private System.Windows.Forms.Label labelM;
+        private System.Windows.Forms.Label labelB;
+        private System.Windows.Forms.Label labelN;
+        private System.Windows.Forms.Label yellowText;
+        private System.Windows.Forms.Label labelDebug;
+        private System.Windows.Forms.Label labelImage;
+    }
+}
+
---
+++ MultiChoiceAdventure/Form1.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiChoiceAdventure
+{
+    public partial class Form1 : Form
+    {
+        int scene = 0;
+        bool keyPresent = false;
+        Random randGen = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+
+            //
+
+            outputLabel.Text = "You begin daydreaming in class, and wake up to a sudden snap of a ruler on your desk. " +
+                "\nYou open your eyes, and everyone is missing, with no trace.";
+            blueText.Text=  "Investigate room.";
+            redText.Text=   "Leave Immediately.";
+            yellowText.Text="N/A";
+            labelDebug.Text = "" + (scene);
+            labelImage.Image = Properties.Resources.Classroom;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int randNum = randGen.Next(1, 11);
+            if (e.KeyCode == Keys.M) //Red 
+            {
+                if (scene == 0)
+                {
+                    scene = 1;
+                    outputLabel.Text = "You run out through the door, looking both ways down the hall," +
+                        "\nstill seeing no one around.";
+                    redText.Text = "Continue.";
+                    blueText.Text = "N/A";
+                    labelImage.Image = Properties.Resources.hallway;
+                }
+                else if (scene == 1 | scene == 4)
+                {
+                    labelImage.Image = Properties.Resources.field;
+                    scene = 5;
+                    outputLabel.Text = "You continue down the hall, and exit the building. " +
+                        "\nThe door leaves you in a random field, and the door is gone when you turn around to look.";
+                    yellowText.Text = "N/A";
+                    redText.Text = "Continue traveling straight forward";
+                    blueText.Text = "Observe surrounding area";
+                }
+                else if (scene == 2)
+                {
+                    scene = 0;
+                    outputLabel.Text = "Obtained key.";
+                    redText.Text = "Continue.";
+                    blueText.Text = "N/A";
+                    keyPresent = true;
+                }
+                else if (scene == 5)
+                {
+                    scene = 6;
+                    outputLabel.Text = "You trip on some sort of root, and close your eyes. " +
+                        "\nWhen it feels you’re about to hit the ground, you notice the sensation of high gusts of wind. " +
+                        "\nYou’re now on a snowy mountain. Somehow";
+                    labelImage.Image = Properties.Resources.snow;
+                    redText.Text = "Continue.";
+                    blueText.Text = "N/A";
+                }
+                else if (scene == 6)
+                {
+                    scene = 8;
+                    outputLabel.Text = "You enter a nearby structure to escape the cold, and come across an armory, " +
+                        "\nwith a locked door on the one side.";
+                    redText.Text = "Browse Equipment";
+                    blueText.Text = "Attempt to unlock door";
+                    labelImage.Image = Properties.Resources.armory;
+                }
+                else if (scene == 7)
+                {
+                    scene = 99;
+                    outputLabel.Text = "You blink, and you’re fully awake in the classroom. You’re sitting straight up, and looking at the board. " +
+                        "\nYou continue your day, forgetting your daydream.";
+                    redText.Text = "Continue.";
+                    blueText.Text = "N/A";
+                    labelImage.Image = Properties.Resources.Classroom;
+                }
+                else if (scene == 8)
+                {
+                    scene = 10;
+                    outputLabel.Text = "You assume you’ll probably need some level of protection, " +
+                        "\nso you equip a basic set of armor from the wall, and grab an odd looking sword" +
+                        "\n from the wall with a trigger.";
+                    redText.Text = "Exit.";
+                    blueText.Text = "Explore.";
+                    labelImage.Image = Properties.Resources.armory;
+                }
+                else if (scene == 9)
+                {
+                    scene = 7;
+                    outputLabel.Text = "You're blinded by a bright light, coming through the door.";
+                    redText.Text = "Continue.";
+                    blueText.Text = "N/A";
+                    labelImage.Image = null;
+
+                }
+                else if (scene == 10)
+                {
+                    scene = 11;
+                    outputLabel.Text = "You exit the building back out into the snow. You suddenly get an odd feeling, and look behind you.";
+                    redText.Text = "Continue.";
+                    blueText.Text = "N/A";
+                    labelImage.Image = Properties.Resources.snow;
+                }
+                else if (scene == 11)
+                {
+                    scene = 12;
+                    outputLabel.Text = "A figure approaches, and takes a stance a distance away.";
+                    redText.Text = "First Strike.";
+                    blueText.Text = "Defend.";
+                }
+                else if (scene == 12)
+                {
+                    scene = 15;
+                    outputLabel.Text = "You successfully land a first hit on the figure, and it retreats to a safe distance.";
+                    redText.Text = "Second hit.";
+                    blueText.Text = "Retreat.";
+                }
+                else if (scene == 13)
+                {
+                    scene = 14;
+                    outputLabel.Text = "You continue down a wide corridor which leads outside, and notice a figure waiting for you.";
+                    redText.Text = "First Strike";
+                    blueText.Text = "Defend.";
+                    labelImage.Image = Properties.Resources.snow;
+                }
+
+                else if (scene == 15)
+                {
+                    scene = 16;
+                    outputLabel.Text = "You land a successful second hit, and the figure drops to the ground.";
+                    redText.Text = "Finisher.";
+                    blueText.Text = "Enter nearby cave.";
+                }
+                else if (scene == 16)
+                {
+                    scene = 18;
+                    outputLabel.Text = "You retreat into the cave, and find a place to relax.";
+                    redText.Text = "Meditate.";
+                    blueText.Text = "Sleep.";
+                    labelImage.Image = Properties.Resources.cave;
+                }
+                else if (scene == 17)
+                {
+                    scene = 20;
+                    outputLabel.Text = "You attempt a second hit, knocking the figure to the ground.";
+                    redText.Text = "Finisher.";
+                    blueText.Text = "Enter nearby cave.";
+                }
+                else if (scene == 18)
+                {
+                    scene = 99;
+                    outputLabel.Text = "You wake up in bed, as it was all a dream.";
+                    redText.Text = "Continue.";
+                    blueText.Text = "N/A";
+                    labelImage.Image = Properties.Resources.bed;
+                }
+                else if (scene == 20)
+                {
+                    scene = 99;
+                    outputLabel.Text = "You enter the cave to the point where you can’t see, and wake up in bed.";
+                    redText.Text = "Continue.";
+                    blueText.Text = "N/A";
+                    labelImage.Image = Properties.Resources.bed;
+                }
+                else if (scene == 21)
+                {
+                    scene = 99;
+                    outputLabel.Text = "You strike your foe one last time, and get knocked out by the force of an explosion. " +
+                        "\nYou sit up straight at your desk, and catch up with the class.";
+                    redText.Text = "Continue.";
+                    blueText.Text = "N/A";
+                    labelImage.Image = Properties.Resources.Classroom;
+                }
+                else if (scene == 22)
+                {
+                    scene = 99;
+                    outputLabel.Text = "You go back to sleep, and wake up at the end of the class.";
+                    redText.Text = "Continue.";
+                    blueText.Text = "N/A";
+                    labelImage.Image = Properties.Resources.Classroom;
+                }
+                else if (scene == 23)
+                {
+                    scene = 7;
+                    outputLabel.Text = "Figure gets around you block and knocks you out.";
+                    redText.Text = "Continue.";
+                    blueText.Text = "N/A";
+                    labelImage.Image = null;
+                }
+
+                else if (scene == 99)
+                {
+                    scene = 100;
+                    labelImage.Image = null;
+                    outputLabel.Text = "Restart?";
+                    redText.Text = "Yes.";
+                    blueText.Text = "No.";
+                }
+                else if (scene == 100)
+                {
+                    outputLabel.Text = "You begin daydreaming in class, and wake up to a sudden snap of a ruler on your desk. " +
+                   "\nYou open your eyes, and everyone is missing, with no trace.";
+                    blueText.Text = "Investigate room.";
+                    redText.Text = "Leave Immediately.";
+                    yellowText.Text = "N/A";
+                    labelDebug.Text = "" + (scene);
+                    labelImage.Image = Properties.Resources.Classroom;
+                    scene = 0;
+                }
+            }
+            if (e.KeyCode == Keys.B)
+            {
+                if (scene == 0)
+                {
+                    scene = 2;
+                    outputLabel.Text = "You walk up and down between the rows of desks, looking for some kind of clue. " +
+                        "\nYou hear the click of the room’s door lock.";
+                    yellowText.Text = "Go back to sleep.";
+                    redText.Text = "Investigate more, then attempt to bust door open.";
+                    blueText.Text = "Enter ventilation system.";
+                }
+                else if (scene == 2)
+                {
+                    scene = 4;
+                    outputLabel.Text = "The vent comes off easily, and you enter the ventilation system.";
+                    yellowText.Text = "N/A";
+                    redText.Text = "Exit into hallway.";
+                    blueText.Text = "Continue through vents.";
+                }
+                else if (scene == 4)
+                {
+                    scene = 7;
+                    outputLabel.Text = "You keep travelling down the shaft, and it gets darker and darker.";
+                    yellowText.Text = "N/A";
+                    redText.Text = "Continue.";
+                    blueText.Text = "N/A";
+                }
+                else if (scene == 5|scene == 9|scene == 23)
+                {
+                    scene = 7;
+                }
+                else if (scene == 8)
+                {
+                    if (keyPresent == true)
+                    {
+                        scene = 9;
+                        outputLabel.Text = "The key opens the door.";
+                        redText.Text = "Continue.";
+                        blueText.Text = "N/A";
+                    }
+                    else
+                    {
+                        scene = 10;
+                        outputLabel.Text = "The door doesn't budge. You get some equipment ";
+                        redText.Text = "Continue.";
+                        blueText.Text = "N/A";
+                    }
+                }
+                else if (scene == 10)
+                {
+                    scene = 13;
+                    outputLabel.Text = "You step into what seems to be a practice floor in a facility.";
+                    redText.Text = "Continue.";
+                    blueText.Text = "N/A";
+                }
+
+                else if (scene ==12|scene==14)
+                {
+                    if (randNum < 7)
+                    {
+                        scene = 20;
+                        outputLabel.Text = "You successfully block and counterattack, kcocking your opponent to the ground.";
+                        redText.Text = "Enter Cave.";
+                        blueText.Text = "Final Strike.";
+
+                    }
+                    else
+                    {
+                        scene = 7;
+                        outputLabel.Text = "Your opponent gets around your block, and knocks you out.";
+                        redText.Text = "Continue.";
+                        blueText.Text = "N/A";
+                    }
+                }
+                else if (scene == 15)
+                {
+                    scene = 17;
+                    outputLabel.Text = "You attempt a second hit and the figure.";
+                    redText.Text = "Continue.";
+                    blueText.Text = "N/A";
+                }
+                else if (scene == 17)
+                {
+                    scene = 21;
+                    outputLabel.Text = "You strike the figure, and you feel the force of an explosion.";
+                    redText.Text = "Continue.";
+                    blueText.Text = "N/A";
+                }
+                else if (scene == 16)
+                {
+                    scene = 19;
+                    outputLabel.Text = "You wake up in class, ruler on your desk, with the teacher beside your desk";
+                    redText.Text = "Continue.";
+                    blueText.Text = "N/A";
+                }
+                else if (scene == 99)
+                {
+                    scene = 99;
+                    labelImage.Image = null;
+                    outputLabel.Text = "Restart?";
+                    redText.Text = "Yes.";
+                    blueText.Text = "No.";
+                }
+                else if (scene ==100)
+                {
+                    Application.Exit();
+                }
+            }
+            if (e.KeyCode == Keys.N)
+            {
+                if (scene == 2)
+                {
+                    scene = 7;
+                    outputLabel.Text = "You go back to sleep, deciding not to explore.l";
+                    yellowText.Text = "N/A";
+                    redText.Text = "Continue.";
+                    blueText.Text = "N/A";
+                }
+            }
+            labelDebug.Text = "" + (scene);
+            Refresh();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
